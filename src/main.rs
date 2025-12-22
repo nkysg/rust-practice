a9e953a6dd14c4e1f49451691d36b685cad7e54c
nk_ysg <nk_ysg@163.com>
2025-12-23 07:03:16 +0800

    use anyhow

--- src/main.rs
+++ src/main.rs
@@ -180,6 +180,23 @@ async fn send_values() {
     drop(tx);
 }
 
-fn main() {
+fn main9() {
     block_on(send_values());
 }
+
+use anyhow::Result;
+use serde_json::Value;
+use std::fs;
+
+fn read_json(path: &str) -> Result<Value> {
+    let content = fs::read_to_string(path)?;
+    let json = serde_json::from_str(&content)?;
+    Ok(json)
+}
+
+fn main() -> Result<()> {
+    let data = read_json("config.json");
+    println!("json {:?}", data);
+    Ok(())
+}
+
